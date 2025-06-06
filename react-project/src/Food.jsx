f59c35d02d24d565f4d6ed0d6f8e8363b195e629
Crawsectus <julioalejandro.7734@gmail.com>
2025-06-06 14:57:16 -0500

    First approach

---
+++ react-project/src/Food.jsx
@@ -0,0 +1,11 @@
+function Food(){
+    const food1="Empanada";
+    const food2="Arroz";
+    return(
+        <ul>
+            <li> {food1} </li>
+            <li> {food2} </li>
+        </ul>
+    );
+}
+export default Food
---
+++ react-project/src/Footer.jsx
@@ -0,0 +1,8 @@
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Alejandro Peñaloza</p>
+        </footer>
+    );
+}
+export default Footer
