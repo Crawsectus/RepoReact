8df615157daf2ed8af4c451e0974d1b291dc4588
Crawsectus <julioalejandro.7734@gmail.com>
2025-06-06 21:10:40 -0500

    Mini avance

---
+++ react-project/src/Card.jsx
@@ -0,0 +1,10 @@
+function Card(){
+    return(
+        <div className="card">
+            <img src="" alt="" />
+            <h2>React</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    );
+}
+export default Card;
