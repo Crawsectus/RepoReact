6f0b9175ae316177c7f6da3d96f6381372265118
Crawsectus <julioalejandro.7734@gmail.com>
2025-06-10 14:04:39 -0500

    Click events

--- react-project/src/Button/Button.jsx
+++ react-project/src/Button/Button.jsx
@@ -1,7 +1,8 @@
 import styles from "./Button.module.css"
 function Button(){
+    const handleClick=(e)=>{e.target.textContent="Ouch!"};
     return(
-        <button className={styles.button}>Click me!</button>
+        <button className={styles.button} onClick={(e)=>handleClick(e)}>Click me!</button>
     );
 }
 export default Button
