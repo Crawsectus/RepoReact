42f1c6784dec5df2df9312ad16d99c89f8b8e058
Crawsectus <julioalejandro.7734@gmail.com>
2025-06-14 09:30:12 -0500

    Update arrays

--- react-project/src/Component/Component.jsx
+++ react-project/src/Component/Component.jsx
@@ -1,7 +1,9 @@
+import { element, func } from "prop-types";
 import React,{useState} from "react";
 function Component(){
     const [name,setName] = useState(0);
     const [car,setCar] = useState({year: 1986,make: "Toyota",model: "Corolla"});
+    const [lista,setLista]=useState(["Hola","Ciao","Hello"]);
     function handleYearChange(event){
         setCar(c=>({...c, year:event.target.value}));
     }
@@ -11,6 +13,14 @@ function Component(){
     function handleModelChange(event){
         setCar(c=>({...c, model:event.target.value}));
     }
+    function handleNewElement(){
+        const nuevoItem=document.getElementById("elementoInput").value;
+        document.getElementById("elementoInput").value="";
+        setLista(l=>[...l,nuevoItem]);
+    }
+    function handleRemove(index){
+        setLista(lista.filter((_,i)=>i!==index));
+    }
     return(
         <div>
             <p>Value: {name}</p>
@@ -18,7 +28,11 @@ function Component(){
             <p>El mejor carro del mundo entero es el: {car.make} {car.model} de {car.year}</p>
             <input type="number" value={car.year} onChange={handleYearChange} />
             <input type="text" value={car.make} onChange={handleMakeChange} />
-            <input type="text" value={car.model} onChange={handleModelChange} />
+            <input type="text" value={car.model} onChange={handleModelChange} /> <br />
+            <h2>Lista:</h2>
+            <ul>{lista.map((elemento,index)=> <li onClick={()=>handleRemove(index)} key={index}>{elemento}</li>) }</ul>
+            <input type="text" id="elementoInput" placeholder="Enter new item"/>
+            <button onClick={handleNewElement}>Add new element</button>
         </div>
     );
 }
